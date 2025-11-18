0c74952ec3ed996d8748b937b5f6e78177e28f2b
Andrew <andrewalyuk@gmail.com>
2025-11-18 18:51:45 +0800

    fully deleted "iter" dep

--- vendor/github.com/eterline/somedata/deque.go
+++ vendor/github.com/eterline/somedata/deque.go
@@ -6,7 +6,6 @@ package somedata
 
 import (
 	"fmt"
-	"iter"
 )
 
 const dequeMinCapacity = 15
@@ -104,29 +103,6 @@ func (q *deque[T]) PopBack() T {
 	return ret
 }
 
-// IterPopBack returns an iterator that iteratively removes items from the back
-// of the deque. This is more efficient than removing items one at a time
-// because it avoids intermediate resizing. If a resize is necessary, only one
-// is done when iteration ends.
-func (q *deque[T]) IterPopBack() iter.Seq[T] {
-	return func(yield func(T) bool) {
-		if q.Len() == 0 {
-			return
-		}
-		var zero T
-		for q.count != 0 {
-			q.tail = q.prev(q.tail)
-			ret := q.buf[q.tail]
-			q.buf[q.tail] = zero
-			q.count--
-			if !yield(ret) {
-				break
-			}
-		}
-		q.shrinkToFit()
-	}
-}
-
 // Front returns the element at the front of the queue. This is the element
 // that would be returned by PopFront. This call panics if the queue is empty.
 func (q *deque[T]) Front() T {
@@ -170,46 +146,6 @@ func (q *deque[T]) Set(i int, item T) {
 	q.checkRange(i)
 	// bitwise modulus
 	q.buf[(q.head+i)&(len(q.buf)-1)] = item
-}
-
-// Iter returns a go iterator to range over all items in the Deque, yielding
-// each item from front (index 0) to back (index Len()-1). Modification of
-// Deque during iteration panics.
-func (q *deque[T]) Iter() iter.Seq[T] {
-	return func(yield func(T) bool) {
-		origHead := q.head
-		origTail := q.tail
-		head := origHead
-		for range q.Len() {
-			if q.head != origHead || q.tail != origTail {
-				panic("deque: modified during iteration")
-			}
-			if !yield(q.buf[head]) {
-				return
-			}
-			head = q.next(head)
-		}
-	}
-}
-
-// RIter returns a reverse go iterator to range over all items in the Deque,
-// yielding each item from back (index Len()-1) to front (index 0).
-// Modification of Deque during iteration panics.
-func (q *deque[T]) RIter() iter.Seq[T] {
-	return func(yield func(T) bool) {
-		origHead := q.head
-		origTail := q.tail
-		tail := origTail
-		for range q.Len() {
-			if q.head != origHead || q.tail != origTail {
-				panic("deque: modified during iteration")
-			}
-			tail = q.prev(tail)
-			if !yield(q.buf[tail]) {
-				return
-			}
-		}
-	}
 }
 
 // Clear removes all elements from the queue, but retains the current capacity.
